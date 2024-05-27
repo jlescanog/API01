709738b711b91e157ffa1e8398562bc479534148
Jhair L. Aorus <jlescanog@outlook.com>
2024-05-26 23:35:43 -0500

    Agregar archivos de proyecto.

---
+++ API01/Controllers/PersonaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ListarPersona()
+        {
+            return Ok("aqui retorno la lista de personas");
+        }
+
+    }
+}
