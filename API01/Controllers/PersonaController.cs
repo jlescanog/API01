2d81a5227dd57e7fd0cad19bda325c295ae0a725
Jhair W L <jlescanog@outlook.com>
2024-05-27 17:42:05 -0500

    Min 22:33 del 06-02-24 IC

--- API01/Controllers/PersonaController.cs
+++ API01/Controllers/PersonaController.cs
@@ -1,17 +1,58 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API01.Modelos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
 
 namespace API01.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    
+    //controlador en especifico
     public class PersonaController : ControllerBase
     {
+        //Metodo/ verbo de http
         [HttpGet]
         public IActionResult ListarPersona()
         {
-            return Ok("aqui retorno la lista de personas");
+            //instanciar
+            List<Persona> lstPersonas = new List<Persona>();
+
+            for (int i = 0; i < 10; i++) { 
+                
+                Persona tmp = new Persona();
+                tmp.Id = i;
+                tmp.Nombre = $"Nombre ${i*3}";
+                tmp.ApellidoPaterno = $"Apellido paterno ${i*3}";
+                tmp.ApellidoMaterno = $"Apellido materno ${i * 3}";
+                tmp.FechaNacimiento = DateTime.Now.AddYears(-i*2);
+                tmp.Direccion = $"Dirección ${i * 7}"; 
+                lstPersonas.Add(tmp);
+            }
+            return Ok(lstPersonas);
         }
 
     }
 }
+
+        ////Metodo/ verbo de http
+        //[HttpGet("Lista-total-persona")]
+        //public IActionResult ListarPersonas()
+        //{
+        //    //instanciar
+        //    List<Persona> lstPersonas = new List<Persona>();
+
+        //    for (int i = 0; i < 20; i++)
+        //    {
+
+        //        Persona tmp = new Persona();
+        //        tmp.Id = i;
+        //        tmp.Nombre = $"Nombre ${i * 3}";
+        //        tmp.ApellidoPaterno = $"Apellido paterno ${i * 3}";
+        //        tmp.ApellidoMaterno = $"Apellido materno ${i * 3}";
+        //        tmp.FechaNacimiento = DateTime.Now.AddYears(-i * 2);
+        //        tmp.Direccion = $"Dirección ${i * 7}";
+        //        lstPersonas.Add(tmp);
+        //    }
+        //    return Ok(lstPersonas);
+        //}
---
+++ API01/Modelos/Persona.cs
@@ -0,0 +1,15 @@
+﻿namespace API01.Modelos
+{
+    public class Persona
+    {
+        public int Id { get; set; } 
+        public string Nombre { get; set; } = "";
+        public string ApellidoPaterno { get; set; } = "";
+        public string ApellidoMaterno { get; set; } = "";
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; } = "";
+
+
+
+    }
+}
